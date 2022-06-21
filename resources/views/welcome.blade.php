019551d27490d24b62e39ad558a2ca3d5cf9b747
LeTruyen <lethithanhtruyen99@gmail.com>
2022-06-21 09:33:27 +0700

    c4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,6 +73,9 @@
                 $treasure_hunt = ["garbage", "cat", 99, ["soda can", 8, ":)", "sludge", ["stuff", "lint", ["GOLD!"], "cave", "bat", "scorpion"], "rock"], "glitter", "moonlight", 2.11];
 
                 echo "\nWe found " . $treasure_hunt[3][4][2][0];
+
+                
     ?>
+    <h1>hello php</h1>
    </body>
 </html>
